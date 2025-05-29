9f3b3f92f0957991c2bd6bde3998a6c4148ae746
Kieran Spruce <kspruce@lcn.ucl.ac.uk>
2025-05-29 14:15:16 +0100

    Applikku Update 4

--- app/models/user.py
+++ app/models/user.py
@@ -4,6 +4,8 @@ from flask_login import UserMixin
 from datetime import datetime
 
 class User(UserMixin, db.Model):
+    __tablename__ = 'users'  # Changed from 'user' to 'users'
+    
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
