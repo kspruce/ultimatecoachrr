72343b60c245a65beabfae31a06906d39d93458f
Kieran Spruce <kspruce@lcn.ucl.ac.uk>
2025-05-29 15:18:14 +0100

    Applikku Update 6.1

--- run.py
+++ run.py
@@ -12,6 +12,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = create_app()
+application = app  # for WSGI servers
 
 @app.route('/debug-info')
 def debug_info():
