9892f1bd6faada9f46592b98e9b1bf07355d954a
Kieran Spruce <kspruce@lcn.ucl.ac.uk>
2025-06-12 15:47:31 +0100

    Fix login

--- app/models/user.py
+++ app/models/user.py
@@ -7,14 +7,13 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    # Increase the length to 255 to accommodate scrypt hashes
     password_hash = db.Column(db.String(255))
+    role = db.Column(db.String(20), default='user')  # Make sure this exists
     is_admin = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     
-    # The relationship will be handled from the Player model
     player_profile = db.relationship('Player', back_populates='user_account', uselist=False)
-
+    
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
