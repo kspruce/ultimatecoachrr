9c627a53b0336afdde62fc499eed3f01fe0b7960
Kieran Spruce <kspruce@lcn.ucl.ac.uk>
2025-06-13 16:28:51 +0100

    Update Homepage

--- app/routes/team.py
+++ app/routes/team.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_required, current_user
+from flask_wtf import FlaskForm
 from sqlalchemy import text
 from app import db
 from app.models.player import Player
