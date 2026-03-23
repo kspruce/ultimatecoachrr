7548eb76e06f1a858753d8061aba933fa4755e65
Kieran Spruce <kspruce@lcn.ucl.ac.uk>
2026-03-23 14:32:34 +0000

    stat taking update 3

--- migrations/versions/f1a2b3c4d5e6_add_pull_hang_time_fields.py
+++ migrations/versions/f1a2b3c4d5e6_add_pull_hang_time_fields.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'f1a2b3c4d5e6'
-down_revision = 'e3e44e60d100'
+down_revision = '7116f57dd6ec'
 branch_labels = None
 depends_on = None
 
